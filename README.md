261610be9fbfc7b16ada3646dec0cc9319d3a026
Catherine <cpolknytx@gmail.com>
2022-11-02 11:05:46 -0500

    added POST request

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Note Taker Starter Code
 
 use activities 21-22 (Modular Routing) to do this challenge
+
+data persistent activity 19-20
+
+ 
--- server.js
+++ server.js
@@ -1,22 +1,54 @@
 const express = require('express');
 const path = require('path');
+// const colors = require('colors');
+// const dbData = require('./db/db.json');
+
 //fs module
 const fs = require('fs');
 //creating a new express app
 const app = express();
 const PORT = 3001; 
 
+//middleware sends statis resourese that are in public folder
 app.use(express.static('public'));
 
-//GET route to get all the 
-app.get('/api/db', (req, res) => res.json(dbData));
-
-
-
-
-
-
-// Listen for connections
-app.listen(PORT, () =>
-  console.info(`Example app listening at http://localhost:${PORT} 🚀`)
+app.get('/notes', (req, res) => 
+ res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
+
+//POST request 
+app.use('api/notes', (req, res) => {
+  //let the client know that their post received
+  console.info(`${req.method} request received to add a note`);
+
+  //Destructuring assignment for the items in req.body
+  const { title, noteText } = req.body;
+    // If all the required properties are present
+  if (title && text) {
+    const newNote = {
+      title, 
+      noteText
+    };
+
+    const responce = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(responce);
+  } else {
+    res.status(500).json('Error in posting a note');
+  }
+});
+
+
+//starts the servese and run the port.
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
+
+//create a GET route for http://localhost:3001/notes
+
+
+
